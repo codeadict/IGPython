e940ea1e18f1593d1abe0dceaf2d7570923145f0
Dairon Medina <daironm@kruger.com.ec>
2013-07-30 13:57:57 -0500

    Get driver list

--- weighin/models.py
+++ weighin/models.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2013 OpenWeigh.co.uk
 # Developer: Dairon Medina Caro <dairon.medina@gmail.com>
 # Co-Developer Rhys Park <sales@openweigh.co.uk>
-import sys, glob
+import sys, glob, inspect
 import serial
 from django.db import models
 from base.models import ActivableMixin
@@ -36,6 +36,17 @@ def port_list():
     choices = tuple((str(i),ports[i]) for i in range(len(ports)))
     return choices
 
+def drivers_list():
+    """
+    Get a tuple of available drivers
+    """
+    controllers = inspect.getmembers(drivers, inspect.isclass)
+    del controllers[-1]
+
+    driver_tuple = tuple(controllers)
+    return driver_tuple
+
+
 
 class Device(ActivableMixin):
     """
@@ -43,7 +54,9 @@ class Device(ActivableMixin):
     """
     name = models.CharField(_('Device Name'), max_length = 100, blank = True)
     port = models.CharField(_('Port ID'), choices=port_list(), max_length = 255, blank = True)
+    driver = models.CharField(_('Driver'), choices=drivers_list(), max_length=50)
     default = models.BooleanField(_('Is Defaul?'), default=False)
+
 
     class Meta:
         ordering = ('default',)
